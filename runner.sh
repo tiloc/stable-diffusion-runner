1debf5002f594941a5d15e8713308552e9946128
Tilo <tiloc@users.noreply.github.com>
2022-09-04 20:31:31 +0200

    Initial release

---
+++ runner.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+set -e
+
+mydir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Include pashua.sh to be able to use the 2 functions defined in that file
+# shellcheck source=/dev/null
+source "$mydir/pashua.sh"
+
+# Define what the dialog should be like
+# Take a look at Pashua's Readme file for more info on the syntax
+
+conf="
+# Set window title
+*.title = Welcome to Stable Diffusion Runner
+
+# Prompt
+tf.type = textbox
+tf.label = Describe your idea
+tf.width = 310
+tf.height = 200
+tf.mandatory = true
+tf.tooltip = Write an idea, such as the description of a scene
+
+
+# Add a cancel button with default label
+cb.type = cancelbutton
+cb.tooltip = Cancel and do not run
+
+db.type = defaultbutton
+db.label = Run
+db.tooltip = Click to run
+"
+
+if [ -d '/Volumes/Pashua/Pashua.app' ]
+then
+	# Looks like the Pashua disk image is mounted. Run from there.
+	customLocation='/Volumes/Pashua'
+else
+	# Search for Pashua in the standard locations
+	customLocation=''
+fi
+
+locate_pashua "$customLocation"
+pashua_run "$conf" "$customLocation"
+
+echo "${tf:?}" > /dev/null
+echo "${cb:?}" > /dev/null
+
+if [[ "$cb" == "1" ]]
+then
+echo "Cancelled - Exit"
+exit 130
+fi
+
+outdir=$(pwd)/../stable-diffusion-output
+mkdir -p "$outdir"
+
+pushd ../stable-diffusion || exit
+# shellcheck source=/dev/null
+source venv/bin/activate
+python scripts/txt2img.py --prompt "$tf" --n_samples 1 --n_iter 1 --plms --outdir "$outdir"
+popd || exit
+
+unset -v latest
+for file in "$outdir"/*; do
+  [[ $file -nt $latest ]] && latest=$file
+done
+
+echo "Image saved to $latest"
+open "$latest"
