2ac6a5dd1a35c1234a4870cb7a987f316d9550c0
Tilo <tiloc@users.noreply.github.com>
2022-09-04 20:54:02 +0200

    Select number of sampling iterations

--- runner.sh
+++ runner.sh
@@ -23,6 +23,16 @@ tf.height = 200
 tf.mandatory = true
 tf.tooltip = Write an idea, such as the description of a scene
 
+# Add a samplingup menu
+sampling.type = popup
+sampling.label = Sampling steps
+sampling.width = 310
+sampling.option = 1
+sampling.option = 10
+sampling.option = 30
+sampling.option = 50
+sampling.default = 30
+sampling.tooltip = How many sampling steps for PLMS?
 
 # Add a cancel button with default label
 cb.type = cancelbutton
@@ -46,6 +56,7 @@ locate_pashua "$customLocation"
 pashua_run "$conf" "$customLocation"
 
 echo "${tf:?}" > /dev/null
+echo "${sampling:?}" > /dev/null
 echo "${cb:?}" > /dev/null
 
 if [[ "$cb" == "1" ]]
@@ -60,7 +71,7 @@ mkdir -p "$outdir"
 pushd ../stable-diffusion || exit
 # shellcheck source=/dev/null
 source venv/bin/activate
-python scripts/txt2img.py --prompt "$tf" --n_samples 1 --n_iter 1 --plms --outdir "$outdir"
+python scripts/txt2img.py --prompt "$tf" --n_samples 1 --n_iter 1 --plms --ddim_steps "$sampling" --outdir "$outdir"
 popd || exit
 
 unset -v latest
